a1c9c0502fe4ab741ff5694805048febc6513178
Venkata Rajesh chundru <vrchundr@mtu.edu>
2020-03-26 22:56:57 -0500

    Added SIR model

---
+++ SIR_Model.m
@@ -0,0 +1,34 @@
+function res = SIR_model()
+close all;
+%%%%%%%%%%%%%%%%%%%%%% Governing Equations %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Function to calculate spread rate based on SIR model
+% https://services.math.duke.edu/education/ccp/materials/diffcalc/sir/sir2.html
+% S' = -aSI
+% I' = asI - bI 
+% R' = bI
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Intialization of variables
+a = 1; % Days before next infection (Days)
+b = 1; % Incubation period (Days)
+
+S0 = 4;% Initial population size (-)
+I0 = 0.1;% Initial infected population (-)
+R0 = 0;% Initial recovered population (summation of recovered and dead)
+           % Assuming dead bodies cannot infect the population (-)
+t_start = 0; % Start time (Days)
+t_final = 6;% End time   (Days)
+%% Main solver
+f=@(t,x) [ -a*x(1)*x(2);a*x(1)*x(2) - b*x(2);b*x(2)]; % Objective function
+
+[t,xa] = ode45(f,[t_start,t_final],[S0 I0 R0]); % Solver
+
+%% Post Processing 
+figure;
+plot(t,xa(:,1),'LineWidth',2);
+hold on;
+grid on;
+plot(t,xa(:,2),'LineWidth',2);
+plot(t,xa(:,3),'LineWidth',2);
+legend('Suseptable population','Infected','Recovered','Location','best');
+res = 0;
+end
