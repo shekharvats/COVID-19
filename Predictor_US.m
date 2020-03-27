5a9929206861ca74adbab5c84cb44f2ac3cf83e8
Shekhar Vats <33394224+shekharvats@users.noreply.github.com>
2020-03-26 21:51:10 -0500

    Add files via upload
    updated data analysis, data plots
    
    -Shekhar

--- Predictor_US.m
+++ Predictor_US.m
@@ -1,14 +1,16 @@
 clc;
 preprocessing
 close all;
-a = 0.1933; % fro US Fit data
-b = 4.85; % from US Fit data
+a =11.66; % from US Fit data
+b = 5.252; % from US Fit data
 
-[x,y]= US_fit([time], US);
+[case_US, stats_case, death_US, stats_death, recovery_US, stats_recovery]  = US_fit(time', US', US_deaths', US_recovery');
 
-time2 = [time,64:80];
-y1 = x(time2);
-
+time2 = [time,size(T,2):size(T,2)+10];
+y1 = case_US(time2); % cases prediction
+y2 = death_US(time2); % deaths prediction
+y3 = recovery_US(time2); % recovery prediction
+%% cases prediction
 figure;
 plot(time2,y1,'--o','LineWidth',2);
 hold on;
@@ -21,6 +23,23 @@ xlabel('Time (Days)');
 ylabel('Number of Confirmed Cases (-)');
 title('Covid 19 number of cases projected (March 24 2020)');
 
+%% deaths prediction
 
+figure;
+plot(time2,y2,'--o','LineWidth',2); hold on; grid on;
+plot(time,US_deaths,'*');
+legend('US Projected','US present','Location','best');
+set(gca,'FontSize',12); set(gca,'FontWeight','bold');
+xlabel('Time (Days)'); ylabel('Number of Confirmed Deaths (-)');
+title('Covid 19 number of deaths projected (March 26 2020)');
+
+%% recovery prediction
+% % % figure;
+% % % plot(time2,y3,'--o','LineWidth',2); hold on; grid on;
+% % % plot(time,US_recovery,'*');
+% % % legend('US Projected','US present','Location','best');
+% % % set(gca,'FontSize',12); set(gca,'FontWeight','bold');
+% % % xlabel('Time (Days)'); ylabel('Number of Confirmed Recovery (-)');
+% % % title('Covid 19 number of recovery projected (March 26 2020)');
 
 
--- US_fit.m
+++ US_fit.m
@@ -1,4 +1,4 @@
-function [fitresult, gof] = US_fit(time, US)
+function [case_US, gof, death_US, gof1, recovery_US, gof2] = US_fit(time, cases, deaths, recovery)
 %CREATEFIT(TIME,US)
 %  Create a fit based on the data from the US
 %
@@ -13,8 +13,13 @@ function [fitresult, gof] = US_fit(time, US)
 % 03/26/2020
 
 %% Fit: 'US_Fit'.
-[xData, yData] = prepareCurveData( time, US ); % preparing data for the model
-
+delta = length(time) - length(recovery); 
+if delta >0
+    recovery = [recovery; zeros(length(delta),1)]; 
+end
+[xData, yData] = prepareCurveData( time, cases ); % preparing data for the model cases
+[pData, qData] = prepareCurveData( time, deaths ); % preparing data for the model deaths
+[mData, nData] = prepareCurveData( time, recovery ); % preparing data for the model recovery
 % Set up fittype and options.
 ft = fittype( 'exp1' );% setting the fit type as exponential
 opts = fitoptions( 'Method', 'NonlinearLeastSquares' );% setting options for the fit
@@ -22,19 +27,40 @@ opts.Algorithm = 'Levenberg-Marquardt'; % fit method type for algorithm
 opts.Display = 'Off'; % turning off display for faster processing
 opts.Normalize = 'on'; %normalizing
 opts.Robust = 'LAR'; 
-opts.StartPoint = [14.6118315670169 5.40706646572049];
+% % % opts.StartPoint = [14.6118315670169 5.40706646572049];
 
 % Fit model to data.
-[fitresult, gof] = fit( xData, yData, ft, opts );% fitting the data based onn actual vs modeled
+[case_US, gof] = fit( xData, yData, ft, opts );% fitting the data 
+[death_US, gof1] = fit( pData, qData, ft, opts );% fitting the data 
+[recovery_US, gof2] = fit( mData, nData, ft, opts );% fitting the data 
 
 % Plot fit with data.
 figure( 'Name', 'US_Fit' );
-h = plot( fitresult, xData, yData );
-legend( h, 'US vs. Time', 'US_Fit', 'Location', 'NorthEast', 'Interpreter', 'none' );
+ax(1) = subplot(3,1,1)
+h = plot( case_US, xData, yData );
+legend( h, 'US COVID Cases vs. Time', 'US_Fit', 'Location', 'NorthEast', 'Interpreter', 'none' );
+% Label axes
+xlabel( 'Time [days]', 'Interpreter', 'none', 'FontSize', 16 );
+ylabel( '# of cases in the US', 'Interpreter', 'none' , 'FontSize', 16 );
+grid on; 
+title ('COVID-19  Cases in the US', 'FontSize', 24 );
+
+ax(2) = subplot(3,1,2)
+h = plot( death_US, pData, qData );
+legend( h, 'US COVID Deaths vs. Time', 'US_Fit', 'Location', 'NorthEast', 'Interpreter', 'none' );
 % Label axes
 xlabel( 'Time [days]', 'Interpreter', 'none', 'FontSize', 16 );
 ylabel( '# of deaths in the US', 'Interpreter', 'none' , 'FontSize', 16 );
 grid on; 
-title ('COVID-19 Projected Deaths in the US', 'FontSize', 24 );
+title ('COVID-19  Deaths in the US', 'FontSize', 24 );
+
+ax(3) = subplot(3,1,3)
+h = plot( recovery_US, mData, nData );
+legend( h, 'US COVID Recovery vs. Time', 'US_Fit', 'Location', 'NorthEast', 'Interpreter', 'none' );
+% Label axes
+xlabel( 'Time [days]', 'Interpreter', 'none', 'FontSize', 16 );
+ylabel( '# of Recovery in the US', 'Interpreter', 'none' , 'FontSize', 16 );
+grid on; 
+title ('COVID-19  Recovery in the US', 'FontSize', 24 );
 
 
