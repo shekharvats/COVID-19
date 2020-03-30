d5b753322c7235f346e72ac73f1f0d2934b3be3f
Venkata Rajesh chundru <vrchundr@mtu.edu>
2020-03-29 20:16:14 -0500

    Added Covid19 death rate prediction to the model based on mortality rate of 3% (WHO march 2020 data)

--- SIR_Model.m
+++ SIR_Model.m
@@ -3,9 +3,10 @@ close all;
 %%%%%%%%%%%%%%%%%%%%%% Governing Equations %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Function to calculate spread rate based on SIR model
 % https://services.math.duke.edu/education/ccp/materials/diffcalc/sir/sir2.html
-% S' = -aSI
-% I' = asI - bI 
-% R' = bI
+% S' = -aSI     - rate of susptible people
+% I' = asI - bI - rate of infection
+% R' = (1-c)*bI - rate of recovred people
+% D' = cbI      - rate of deaths
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Intialization of variables
 %a = 1; % Days before next infection (Days)
@@ -13,14 +14,15 @@ close all;
 
 S0 = 1;% Initial population size (-)
 I0 = 1.27e-06;% Initial infected population (-)
-R0 = 0;% Initial recovered population (summation of recovered and dead)
-           % Assuming dead bodies cannot infect the population (-)
+R0 = 0;% Initial recovered population (-)     
+D0 = 0; % Intial deaths (-)
+c=0.03; % Mortaility rate (based on WHO assumption of 3% 0 March 2020)
 t_start = 0; % Start time (Days)
 t_final = 149;% End time   (Days)
 %% Main solver
-f=@(t,x) [ -a*x(1)*x(2);a*x(1)*x(2) - b*x(2);b*x(2)]; % Objective function
+f=@(t,x) [ -a*x(1)*x(2);a*x(1)*x(2) - b*x(2);(1-c)*b*x(2);c*b*x(2)]; % Objective function
 
-[t,xa] = ode45(f,[t_start,t_final],[S0 I0 R0]); % Solver
+[t,xa] = ode45(f,[t_start,t_final],[S0 I0 R0 D0]); % Solver
 
 %% Post Processing 
 plot_flag = 0;
